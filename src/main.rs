84f621184b445d425251353d6a1c7edfa3728227
Sagar <sagarxyadav@gmail.com>
2023-02-21 16:19:58 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-// main.rs
+// lib.rs
 
 use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
 use rust_crypto::aes::{Aes128, Aes256};
 use rust_crypto::blockmodes::{EcbEncryptor, EcbDecryptor};
 use rust_crypto::buffer::{ReadBuffer, WriteBuffer, BufferResult};
 
 #[pyfunction]
-fn aes_encrypt(key: &[u8], input: &[u8]) -> PyResult<PyObject> {
+fn aes_encrypt(_py: Python, key: &[u8], input: &[u8]) -> PyResult<PyObject> {
     let mut output = vec![0; input.len()];
     let key = match key.len() {
         16 => Aes128::new(key),
@@ -14,15 +15,15 @@ fn aes_encrypt(key: &[u8], input: &[u8]) -> PyResult<PyObject> {
         _ => return Err(exceptions::ValueError::py_err("Invalid key length")),
     };
     let mut encryptor = EcbEncryptor::new(key, &mut output, PaddingScheme::NoPadding);
-    let mut buffer = RefReadBuffer::new(input);
-    let mut output_buffer = RefWriteBuffer::new(&mut output);
+    let mut buffer = ReadBuffer::new(input);
+    let mut output_buffer = WriteBuffer::new(&mut output);
     let _ = encryptor.encrypt(&mut buffer, &mut output_buffer, true).unwrap();
     output.truncate(output_buffer.position() as usize);
-    Ok(output.into_py(py))
+    Ok(output.into_py(_py))
 }
 
 #[pyfunction]
-fn aes_decrypt(key: &[u8], input: &[u8]) -> PyResult<PyObject> {
+fn aes_decrypt(_py: Python, key: &[u8], input: &[u8]) -> PyResult<PyObject> {
     let mut output = vec![0; input.len()];
     let key = match key.len() {
         16 => Aes128::new(key),
@@ -30,15 +31,15 @@ fn aes_decrypt(key: &[u8], input: &[u8]) -> PyResult<PyObject> {
         _ => return Err(exceptions::ValueError::py_err("Invalid key length")),
     };
     let mut decryptor = EcbDecryptor::new(key, &mut output, PaddingScheme::NoPadding);
-    let mut buffer = RefReadBuffer::new(input);
-    let mut output_buffer = RefWriteBuffer::new(&mut output);
+    let mut buffer = ReadBuffer::new(input);
+    let mut output_buffer = WriteBuffer::new(&mut output);
     let _ = decryptor.decrypt(&mut buffer, &mut output_buffer, true).unwrap();
     output.truncate(output_buffer.position() as usize);
-    Ok(output.into_py(py))
+    Ok(output.into_py(_py))
 }
 
 #[pymodule]
-fn rust_aes(_py: Python, m: &PyModule) -> PyResult<()> {
+fn q365AES(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(aes_encrypt, m)?)?;
     m.add_function(wrap_pyfunction!(aes_decrypt, m)?)?;
     Ok(())
